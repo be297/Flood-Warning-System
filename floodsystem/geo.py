16ac261b50d5285d0727e63ddce4b4641a6e5249
be297 <be297@cam.ac.uk>
2023-01-21 17:50:07 +0000

    Completed the two functions for task D

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -7,3 +7,21 @@ geographical data.
 """
 
 from .utils import sorted_by_key  # noqa
+from floodsystem.stationdata import build_station_list
+
+#Task 1D-1, this function returns the name of rivers with at least one monitoring station in a set
+def rivers_with_station(stations):
+    rivers = set()
+    for i in stations:
+        rivers.add(i.river)
+    return rivers
+
+# Task 1D-2, this function returns a dictionary with rivers as the key and station objects as the items 
+def stations_by_river(stations):
+  rivers = (rivers_with_station(stations))
+  rivers_dict = {} # Creating empty dictionary
+  for i in rivers: # Creating empty lists with the rivers as keys
+    rivers_dict[i] = []
+  for i in stations:
+    rivers_dict[i.river].append(i)
+  return rivers_dict
